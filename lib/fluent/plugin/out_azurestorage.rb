6118da38aeff84519c059c85198fbef3a7d20878
Ting-Kuan Wu <tkwu@cloudera.com>
2018-12-12 00:24:24 -0800

    recover failure message.

--- lib/fluent/plugin/out_azurestorage.rb
+++ lib/fluent/plugin/out_azurestorage.rb
@@ -189,7 +189,7 @@ module Fluent::Plugin
         "curl -s " + 
         "'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fstorage.azure.com%2F'" +
         "-H Metadata:true | jq -r '.access_token'")
-      raise "Failed to acquire access token: #{stdout} #{stderr} #{status}" unless status.success?
+      raise "Failed to acquire access token(#{status}): #{stderr}" unless status.success?
 
       return stdout
     end
